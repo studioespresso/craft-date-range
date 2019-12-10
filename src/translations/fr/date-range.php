3ece0826abef7b45b7acd41360401051406cb5e7
Jan Henckens <jan@studioespresso.co>
2019-12-10 20:01:11 +0100

    Added fr & nl translations #6

---
+++ src/translations/fr/date-range.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Navigate plugin for Craft CMS 3.x
+ *
+ * Navigation plugin for Craft 3
+ *
+ * @link      https://studioespresso.co
+ * @copyright Copyright (c) 2018 Studio Espresso
+ */
+
+/**
+ * Navigate en Translation
+ *
+ * Returns an array with the string to be translated (as passed to `Craft::t('navigate', '...')`) as
+ * the key, and the translation as the value.
+ *
+ * http://www.yiiframework.com/doc-2.0/guide-tutorial-i18n.html
+ *
+ * @author    Studio Espresso
+ * @package   Navigate
+ * @since     0.0.1
+ */
+return [
+    'Start date' => 'Date de début',
+    'Start time' => 'Heure de début',
+    'End date' => 'Date de fin',
+    'End time' => 'Heure de fin',
+];
