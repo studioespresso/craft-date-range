b995fcafd761c67b18803936e55a6c09b50471bc
Samuel Fischer <samuel@digitalnatives.nl>
2021-11-17 17:12:03 +0100

    Added possiblity to add query arguments

---
+++ src/gql/arguments/EntriesArguments.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace studioespresso\daterange\gql\arguments;
+
+use craft\gql\arguments\elements\Entry;
+use craft\gql\types\QueryArgument;
+use GraphQL\Type\Definition\Type;
+
+/**
+ * Add isFuture / isPast / isOngoing to the entry queries
+ *
+ * Class EntriesArguments
+ * @package studioespresso\daterange\gql\arguments
+ */
+class EntriesArguments extends Entry
+{
+    /**
+     * Create custom arguments to add to element query
+     *
+     * @return array
+     */
+    public static function getArguments(): array
+    {
+        return array_merge(parent::getArguments(), self::getContentArguments(), [
+            'isFuture' => [
+                'name' => 'isFuture',
+                'type' => Type::listOf(QueryArgument::getType()),
+                'description' => 'Query entries in the future'
+            ],
+            'isOngoing' => [
+                'name' => 'isOngoing',
+                'type' => Type::listOf(QueryArgument::getType()),
+                'description' => 'Query ongoing entries'
+            ],
+            'isPast' => [
+                'name' => 'isPast',
+                'type' => Type::listOf(QueryArgument::getType()),
+                'description' => 'Query entries in the past'
+            ]
+        ]);
+    }
+}
